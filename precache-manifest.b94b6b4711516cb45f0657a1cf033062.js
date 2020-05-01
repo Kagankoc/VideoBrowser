3c7550fe4102ed312a5c88ae85b9f2eaec0cfa01
Kağan Koç <cemsukagan@hotmail.com>
2020-05-02 00:54:50 +0300

    Updates

---
+++ precache-manifest.b94b6b4711516cb45f0657a1cf033062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f7a5ab8f6d63f31341b910e107d8c6",
+    "url": "/VideoBrowser/index.html"
+  },
+  {
+    "revision": "6a1fc9563961d95cd733",
+    "url": "/VideoBrowser/static/css/main.d4a7af0d.chunk.css"
+  },
+  {
+    "revision": "b0b5a748b20ef05ae160",
+    "url": "/VideoBrowser/static/js/2.7f9c176a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VideoBrowser/static/js/2.7f9c176a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a1fc9563961d95cd733",
+    "url": "/VideoBrowser/static/js/main.020af802.chunk.js"
+  },
+  {
+    "revision": "862baf6f5c05a90d6813",
+    "url": "/VideoBrowser/static/js/runtime-main.096353f1.js"
+  }
+]);
